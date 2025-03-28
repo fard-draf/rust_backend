e279cd12aadf995d3505dc38ab7b0ec98a69c5e9
fard <fard@ik.me>
2025-03-28 15:45:24 +0100

    add routes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 mod models;
 mod routes;
 
-use axum::Router;
+use axum::{Router, routing::get};
+use routes::{health::healthcheck_handler, tasks::tasks_handler};
 use tokio::net::TcpListener;
 
-use crate::routes::create_router;
+// use crate::routes::create_router;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new();
+    let app = Router::new()
+                        .route("/healthcheck", get(healthcheck_handler))
+                        .route("/tasks", get(tasks_handler));
 
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
     println!("Server running on 127.0.0.1:3000.");
